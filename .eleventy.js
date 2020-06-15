8f04239bc07880c803efbbf25243c2e16496bb47
Jason Pamental <jason@rwt.io>
2020-06-15 16:46:24 -0400

    Setting up

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addCollection("posts", function(collection) {
+    const coll = collection.getFilteredByTag("blog");
+  
+    for(let i = 0; i < coll.length ; i++) {
+      const prevPost = coll[i-1];
+      const nextPost = coll[i + 1];
+  
+      coll[i].data["prevPost"] = prevPost;
+      coll[i].data["nextPost"] = nextPost;
+    }
+  
+    return coll;
+  });
+
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  
+
+    
+    return {
+      dir: {
+        // ⚠️ These values are both relative to your input directory.
+        includes: "_includes",
+        layouts: "_includes/_layouts",
+        partials: "_includes/partials",
+        data: "_data",
+      }
+    }
+  
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Building Hope: Documenting the creation of a new web platform for the State of Rhode Island
